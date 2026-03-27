eec484cb17ce72e0c66489fcc4bba5a7ac1431f2
BendeHuba <bende.huba@students.jedlik.eu>
2026-03-27 12:22:00 +0100

    Added #theme to project to point to the style.css file. Header and footer now use #theme. Footer now reacts to theme change

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer {
-
+  theme = inject(ThemeService);
 }
