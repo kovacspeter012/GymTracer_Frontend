b03069c97da40ce02b55433268d172e6279b5dfd
BendeHuba <bende.huba@students.jedlik.eu>
2026-03-27 08:47:14 +0100

    Applied mainlayout in app and app routes #16

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Mainlayout } from './layout/mainlayout/mainlayout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Mainlayout,
+        children: [
+            
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
