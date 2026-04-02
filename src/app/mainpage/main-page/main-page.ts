dbec8eff69b0b18d638fd35fa73984d2c2db32f8
Péter <kovacs.peter@students.jedlik.eu>
2026-04-02 16:14:29 +0200

    ticket table and mainpage updated

--- src/app/mainpage/main-page/main-page.ts
+++ src/app/mainpage/main-page/main-page.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { TicketsService } from '../service/ticket-service';
 import { TicketTable } from '../ticket-table/ticket-table';
+import { ThemeService } from '../../services/theme.service';
+import { NgClass } from '@angular/common';
+import { TrainingCards } from '../training-cards/training-cards';
 
 @Component({
   selector: 'app-main-page',
-  imports: [TicketTable],
+  imports: [TicketTable, TrainingCards, NgClass],
   templateUrl: './main-page.html',
   styleUrl: './main-page.css',
 })
 export class MainPage{
-
+  theme = inject(ThemeService)
 }
