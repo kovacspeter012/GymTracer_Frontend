d9862794131159c26718fca7e05e39858d722465
Bende Huba <bende.huba@students.jedlik.eu>
2026-04-03 17:31:45 +0200

    Edited ticket model and training item picture  #7

--- src/app/trainingdetails/models/training.ticket.model.ts
+++ src/app/trainingdetails/models/training.ticket.model.ts
@@ -1,8 +1,10 @@
+import { TicketType } from "./training.ticklet.model";
+
 export type Ticket = {
   id: number;
   description: string;
   isStudent: boolean;
-  type: string;
+  type: TicketType;
   price: number;
   tax_key: string;
   maxUsage: number;
