d695f4ed0e4f04f41592c96c4be434016d15971c
Péter <kovacs.peter@students.jedlik.eu>
2026-04-04 15:49:50 +0200

    hu locale date language support added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,10 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './services/auth.interceptor';
+import { registerLocaleData } from '@angular/common';
+import localHu from '@angular/common/locales/hu';
+
+registerLocaleData(localHu, 'hu');
 
 export const appConfig: ApplicationConfig = {
   providers: [
