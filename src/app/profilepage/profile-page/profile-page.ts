c9a6baed0dbb8ed9fa166674b7ed485539362f39
Péter <kovacs.peter@students.jedlik.eu>
2026-04-04 15:49:30 +0200

    profile page shows profile data

--- src/app/profilepage/profile-page/profile-page.ts
+++ src/app/profilepage/profile-page/profile-page.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { UserdataService } from '../services/userdata-service';
+import { ThemeService } from '../../services/theme.service';
+import { DatePipe, NgClass } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+import { UserProfileModel } from '../userprofile.model.ts/userprofile.model';
+import { CardsList } from '../cards-list/cards-list';
 
 @Component({
   selector: 'app-profile-page',
-  imports: [],
+  imports: [NgClass, DatePipe, CardsList],
   templateUrl: './profile-page.html',
   styleUrl: './profile-page.css',
 })
-export class ProfilePage {
+export class ProfilePage implements OnInit {
+  theme = inject(ThemeService);
+  authService = inject(AuthService);
+  userdataService = inject(UserdataService);
 
+  userData: UserProfileModel | null = null;
+  
+  ngOnInit(): void {
+    this.userdataService.getUserData(this.authService.actingUser!.id).subscribe({
+      next: (res) => {
+        this.userData = res;
+      },
+      error: (error) => {
+        console.log(error.url);
+      }
+    });
+  }
 }
