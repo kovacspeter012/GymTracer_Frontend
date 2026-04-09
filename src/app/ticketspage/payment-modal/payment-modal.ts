b005e471789c71576419522e097c76fdb1ff8fdd
Péter <kovacs.peter@students.jedlik.eu>
2026-04-09 16:22:51 +0200

    payment modal created

---
+++ src/app/ticketspage/payment-modal/payment-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-modal',
+  imports: [],
+  templateUrl: './payment-modal.html',
+  styleUrl: './payment-modal.css',
+})
+export class PaymentModal {
+
+}
