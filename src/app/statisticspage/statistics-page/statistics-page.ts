bad31258316c4675828cf90d6f382b8718c2d0ce
Péter <kovacs.peter@students.jedlik.eu>
2026-04-11 15:34:38 +0200

    statistics page created #13

---
+++ src/app/statisticspage/statistics-page/statistics-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-statistics-page',
+  imports: [],
+  templateUrl: './statistics-page.html',
+  styleUrl: './statistics-page.css',
+})
+export class StatisticsPage {
+
+}
