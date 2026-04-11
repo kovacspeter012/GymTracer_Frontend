cda73de88d44f60a02f940643f900e322abae7f6
Péter <kovacs.peter@students.jedlik.eu>
2026-04-11 17:05:55 +0200

    income page created #14

---
+++ src/app/incomepage/income-page/income-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-income-page',
+  imports: [],
+  templateUrl: './income-page.html',
+  styleUrl: './income-page.css',
+})
+export class IncomePage {
+
+}
