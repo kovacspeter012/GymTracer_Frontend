30de4a605e186df3f9805f271bdd305de81423da
Péter <kovacs.peter@students.jedlik.eu>
2026-04-12 18:58:25 +0200

    cardusage page created

---
+++ src/app/cardusagepage/cardusage-page/cardusage-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cardusage-page',
+  imports: [],
+  templateUrl: './cardusage-page.html',
+  styleUrl: './cardusage-page.css',
+})
+export class CardusagePage {
+
+}
