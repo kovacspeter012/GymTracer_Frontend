1d672361b2f8c12831ab612b84112c0287f85aa8
Bende Huba <bende.huba@students.jedlik.eu>
2026-04-16 20:19:51 +0200

    Profile edit now allows admins to edit the role of the user

--- src/app/profilepage/profile-modification-form/profile-modification-form.ts
+++ src/app/profilepage/profile-modification-form/profile-modification-form.ts
@@ -5,6 +5,7 @@ import { DatePipe, NgClass } from '@angular/common';
 import { FormsModule, NgForm } from '@angular/forms';
 import { UserdataService } from '../services/userdata-service';
 import { AuthService } from '../../services/auth.service';
+import { UserRole } from '../../models/user.role.model';
 
 
 @Component({
@@ -21,14 +22,19 @@ export class ProfileModificationForm implements OnInit {
   authService = inject(AuthService);
   datePipe = inject(DatePipe);
 
+  UserRole = UserRole;
+
   errorMsg: string | null = null;
+  isLoading = false;
 
   @Input() userData: UserProfileModel | null = null;
+  originalUserData: UserProfileModel | null = null;
   userDataDate: string = '';
   @Output() isSuccessful = new EventEmitter<boolean>();
 
   ngOnInit(): void {
     this.userDataDate = this.datePipe.transform(this.userData!.birthDate!, 'yyyy-MM-dd') || '';
+    if(this.userData) this.originalUserData = {...this.userData};
   }
 
   cancel() {
@@ -57,13 +63,58 @@ export class ProfileModificationForm implements OnInit {
       this.userData!.birthDate = new Date(this.userDataDate);
     }
 
-    this.userdataService.modifyUserData(this.authService.actingUser!.id, this.userData!).subscribe({
-      next: (res) => {        
-        this.isSuccessful.emit(true);
-      },
-      error: (error) => {
-        this.errorMsg = error.error.error || error.error.message || "Probléma volt a felhasználó módosítása közben, próbáld meg később!";
-      }
-    });
+    const originalDateStr = this.datePipe.transform(this.originalUserData!.birthDate, 'yyyy-MM-dd') || '';
+    const dateChanged = this.userDataDate !== originalDateStr;
+
+    const dataChanged = this.userData!.name !== this.originalUserData!.name || 
+                        this.userData!.email !== this.originalUserData!.email || 
+                        dateChanged;
+    const roleChanged = this.userData!.role !== this.originalUserData!.role;
+
+    let changesCalled = 0;
+    if (dataChanged) changesCalled++;
+    if (roleChanged) changesCalled++;
+
+    if (changesCalled === 0) {
+      this.isSuccessful.emit(true);
+      return; 
+    }
+
+    this.isLoading = true;
+    let changesSuccessfull = 0;
+
+    if (dataChanged) {
+      this.userdataService.modifyUserData(this.authService.actingUser!.id, this.userData!).subscribe({
+        next: (res) => {
+          changesSuccessfull++;
+          if (changesSuccessfull === changesCalled) {
+            this.isLoading = false;
+            this.isSuccessful.emit(true);
+          }
+        },
+        error: (error) => {
+          this.isLoading = false;
+          this.errorMsg = error.error?.error || error.error?.message || "Probléma volt a felhasználó módosítása közben, próbáld meg később!";
+        }
+      });
+    }
+
+    if (roleChanged) {
+      this.userdataService.modifyUserRole(this.authService.actingUser!.id, this.userData!.role).subscribe({
+        next: (res) => {
+          changesSuccessfull++;
+          if (changesSuccessfull === changesCalled) {
+            this.isLoading = false;
+            this.isSuccessful.emit(true);
+          }
+        },
+        error: (error) => {
+          this.isLoading = false;
+          console.log(error);
+          this.errorMsg = error.error?.error || error.error?.message || "Probléma volt a szerepkör módosítása közben, próbáld meg később!";
+        }
+      });
+    }
+
   }
 }
--- src/app/profilepage/services/userdata-service.ts
+++ src/app/profilepage/services/userdata-service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { UserProfileModel } from '../userprofilemodels/userprofile.model';
+import { UserRole } from '../../models/user.role.model';
 
 @Injectable({
   providedIn: 'root',
@@ -16,6 +17,10 @@ export class UserdataService {
 
   modifyUserData(id: number, data: UserProfileModel) {
     return this.httpService.put<UserProfileModel>(`${this.apiUrl}/User/${id}/profile`, data);
+  }
+
+  modifyUserRole(id:number, role: UserRole){
+    return this.httpService.put(`${this.apiUrl}/User/${id}/role`, {role});
   }
 
   deleteUser(id: number) {
