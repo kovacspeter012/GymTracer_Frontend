c3123ef6374a05df7e52b593bf8207c98c7eeb0b
Bende Huba <bende.huba@students.jedlik.eu>
2026-04-18 15:13:50 +0200

    Footer reworked

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -1,10 +1,11 @@
 import { NgClass } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ThemeService } from '../../services/theme.service';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-footer',
-  imports: [NgClass],
+  imports: [NgClass, RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
